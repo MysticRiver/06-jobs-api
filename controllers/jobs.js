1fa719f6cf80d0f186c3a8a6747ede3f8fda204f
Tarik Carey <renewed@gmail.com>
2025-01-31 14:19:33 -0500

    Assignment Completed
    Jobs API - part 2
    Week 12 of 18
    
    Jan 15, 2025 - Jan 21, 2025

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,10 +3,21 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-    res.send("get all jobs");
+   const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+   res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 }
 const getJob = async (req, res) => {
-    res.send("get job");
+    const { user: { userId },
+            params: { id: jobId },
+        } = req;
+    const job = await Job.findOne({
+        _id: jobId,
+        createdBy: userId,
+    }); 
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`);
+    }
+    res.status(StatusCodes.OK).json({ job });
 }
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userId;
@@ -14,10 +25,35 @@ const createJob = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ job });
 }
 const updateJob = async (req, res) => {
-    res.send("update job");
+    const { 
+        user: { userId },
+        body: { company, position },
+        params: { id: jobId },
+} = req;
+if (company === "" || position === "") {
+    throw new BadRequestError("Company or Position fields cannot be empty");
+}
+const job = await Job.findByIdAndUpdate(
+    { _id: jobId, 
+      createdBy: userId },
+      req.body,
+    { new: true, runValidators: true }
+);
 }
 const deleteJob = async (req, res) => {
-    res.send("delete job");
+    const { 
+        user: { userId },
+        body: { company, position },
+        params: { id: jobId },
+} = req;
+const job = await Job.findOneAndDelete({
+    _id: jobId,
+    createdBy: userId,
+})
+if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+}
+res.status(StatusCodes.OK).send();
 }
 module.exports = {
     getAllJobs,
